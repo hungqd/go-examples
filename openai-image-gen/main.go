32c453ddb70cdb0854f5135e9208d1d37b6ed2fe
Đặng Quang Hưng <hungdq58@gmail.com>
2024-09-20 16:30:50 +0700

    OpenAI Gen Image returns URL

--- openai-image-gen/main.go
+++ openai-image-gen/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/base64"
 	"flag"
 	"fmt"
-	"image/png"
 	"log"
 	"os"
 
@@ -15,7 +12,6 @@ import (
 )
 
 var prompt = flag.String("p", "", "Prompt")
-var outfile = flag.String("o", "", "Output file name")
 
 func main() {
 	flag.Parse()
@@ -34,7 +30,7 @@ func main() {
 	req := openai.ImageRequest{
 		Prompt:         *prompt,
 		Size:           openai.CreateImageSize256x256,
-		ResponseFormat: openai.CreateImageResponseFormatB64JSON,
+		ResponseFormat: openai.CreateImageResponseFormatURL,
 		N:              1,
 	}
 
@@ -44,28 +40,6 @@ func main() {
 		return
 	}
 
-	imgBytes, err := base64.RawStdEncoding.DecodeString(resp.Data[0].B64JSON)
-	if err != nil {
-		fmt.Printf("Base64 decode error: %v\n", err)
-		return
-	}
-
-	r := bytes.NewBuffer(imgBytes)
-	imgData, err := png.Decode(r)
-	if err != nil {
-		fmt.Printf("PNG decode error: %v\n", err)
-		return
-	}
-
-	file, err := os.Create(fmt.Sprintf("%s.png", *outfile))
-	if err != nil {
-		fmt.Printf("File creation error: %v\n", err)
-		return
-	}
-	defer file.Close()
-
-	if err := png.Encode(file, imgData); err != nil {
-		fmt.Printf("PNG encode error: %v\n", err)
-		return
-	}
+	url := resp.Data[0].URL
+	fmt.Println(url)
 }
